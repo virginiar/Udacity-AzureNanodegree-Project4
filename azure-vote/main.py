829fdabca5012ac1540ad3f5402a695db489fdfe
Virginia Redondo <virginia.redondoanton@gmail.com>
2021-10-03 13:30:11 +0200

    AKS

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -87,7 +87,23 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-r = redis.Redis()
+#r = redis.Redis()
+
+# Redis configurations
+redis_server = os.environ['REDIS']
+
+# Redis Connection
+try:
+    if "REDIS_PWD" in os.environ:
+        r = redis.StrictRedis(host=redis_server,
+                        port=6379,
+                        password=os.environ['REDIS_PWD'])
+    else:
+        r = redis.Redis(redis_server)
+    r.ping()
+except redis.ConnectionError:
+    exit('Failed to connect to Redis, terminating.')
+
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
